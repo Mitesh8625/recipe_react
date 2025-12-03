03e10ac33008fbe1420eb71b7a9c5b865631deae
Mitesh Patel <miteshpatel8625@gmail.com>
2025-12-03 10:28:21 +0000

    recipe-5

--- my-app/src/components/Recipe.jsx
+++ my-app/src/components/Recipe.jsx
@@ -1,105 +1,111 @@
 import { useEffect, useState } from "react"
 
-export default function Recipe()
-{
+export default function Recipe() {
 
-    const [recipename,setRecipeName]=useState("");
-    const [ingredients,setIngredients]=useState("");
-     const [instructions,setInstruction]=useState("");
-     const [recipes,setRecipes]=useState([]);
-     const[editingId,setEditingId]=useState(null);
+    const [recipename, setRecipeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [instructions, setInstruction] = useState("");
+    const [recipes, setRecipes] = useState([]);
+    const [editingId, setEditingId] = useState(null);
 
 
-     // Save REcipe to localstorage
-     const saveRecipes=(updatedRecipes)=>{
-        localStorage.setItem("recipes",JSON.stringify(updatedRecipes));
+    // Save REcipe to localstorage
+    const saveRecipes = (updatedRecipes) => {
+        localStorage.setItem("recipes", JSON.stringify(updatedRecipes));
         setRecipes(updatedRecipes);
 
-     }
+    }
 
-     //Load recipes
+    //Load recipes
 
-       useEffect(()=>{
-        const storedRecipes=JSON.parse(localStorage.getItem("recipes"))||[];
+    useEffect(() => {
+        const storedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
         setRecipes(storedRecipes);
-       },[])
+    }, [])
 
-     //add recipes
-     const handleSubmit=(e)=>{
+    //add recipes
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(!recipename  || !ingredients || !instructions)
-        {
+        if (!recipename || !ingredients || !instructions) {
             alert("Please fill all fields");
             return;
         }
 
-        if(editingId)
-        {
-            const updatedRecipes=recipes.map((r)=>
-            r.id===editingId?{...r,name:recipename,ingredients,ingredients} :r
-        )
-        saveRecipes(updatedRecipes);
-        setEditingId(null);
+        if (editingId) {
+            const updatedRecipes = recipes.map((r) =>
+                r.id === editingId ? { ...r, name: recipename, ingredients, ingredients } : r
+            )
+            saveRecipes(updatedRecipes);
+            setEditingId(null);
         }
-        else
-        {const newRecipe={
-            id:Date.now(),
-            name:recipename,
-            ingredients,
-            instructions
-        }
-        saveRecipes([...recipes,newRecipe]);
+        else {
+            const newRecipe = {
+                id: Date.now(),
+                name: recipename,
+                ingredients,
+                instructions
+            }
+            saveRecipes([...recipes, newRecipe]);
 
         }
         setRecipeName("")
         setIngredients("")
         setInstruction("")
-        
-     }
 
-     //Delet Recipe
-     const handleDelete=(id)=>{
-        const updatedRecipes=recipes.filter((r)=>r.id!==id)
+    }
+
+    //Delet Recipe
+    const handleDelete = (id) => {
+        const updatedRecipes = recipes.filter((r) => r.id !== id)
         saveRecipes(updatedRecipes);
-     }
+    }
 
-    return(
+    //Edit Recipe
+    const handleEdit = (r) => {
+        setRecipeName(r.name);
+        setIngredients(r.ingredients);
+        setInstruction(r.instructions);
+        setEditingId(r.id);
+    }
+
+    return (
         <>
-        <div style={{maxWidth:"600px", margin:"20px auto", border:"1px solid #ccc",padding:"30px", background:"lightblue"}}>
-            <h1 style={{marginBottom:"20px", border:"1px solid #ccc",padding:"20px", background:"red"}}>Recipe Book </h1>
-            <form onSubmit={handleSubmit}> 
-                <div style={{marginBottom:"10px"}}>
-                    <label>Name:</label><br />
-                    <input type="text" style={{width:"100%",padding:"5px"}} value={recipename} onChange={(e)=>setRecipeName(e.target.value)}/>
-                </div>
+            <div style={{ maxWidth: "600px", margin: "20px auto", border: "1px solid #ccc", padding: "30px", background: "lightblue" }}>
+                <h1 style={{ marginBottom: "20px", border: "1px solid #ccc", padding: "20px", background: "red" }}>Recipe Book </h1>
+                <form onSubmit={handleSubmit}>
+                    <div style={{ marginBottom: "10px" }}>
+                        <label>Name:</label><br />
+                        <input type="text" style={{ width: "100%", padding: "5px" }} value={recipename} onChange={(e) => setRecipeName(e.target.value)} />
+                    </div>
 
-                <div  style={{marginBottom:"10px"}}>
-                 <label>Ingredients:</label><br />
-                 <input type="text" style={{width:"100%",padding:"5px"}} value={ingredients} onChange={(e)=>setIngredients(e.target.value)} />
-                </div>
+                    <div style={{ marginBottom: "10px" }}>
+                        <label>Ingredients:</label><br />
+                        <input type="text" style={{ width: "100%", padding: "5px" }} value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+                    </div>
 
-                <div style={{marginBottom:"10px"}}>
-                    <label>Instructions:</label><br />
-                    <input type="text" style={{width:"100%",padding:"5px"}} value={instructions} onChange={(e)=>setInstruction(e.target.value)}/>
-                </div>
+                    <div style={{ marginBottom: "10px" }}>
+                        <label>Instructions:</label><br />
+                        <input type="text" style={{ width: "100%", padding: "5px" }} value={instructions} onChange={(e) => setInstruction(e.target.value)} />
+                    </div>
 
-                <button style={{padding:"5px 10px " }} type="submit"> Add Recipe</button>
-            </form>
-        </div>
-          <div style={{maxWidth:"600px", margin:"20px auto", border:"1px solid #ccc",padding:"30px"}}>
-          <h3>All Recipes</h3>
-          {recipes.length===0 && <p>No recipes Added yet</p>}
-          {recipes.map((r)=>(
-            <div key={r.id} style={{border:"1px solid #ccc", padding:"10px",marginBottom:"10px"}}>
-                <h4>Recipe Name :{r.name}</h4>
-                <p><strong>Ingredients:</strong></p>
-                <ul>{r.ingredients}</ul>
-                <p><strong>Instructions:{r.instructions}</strong></p>
-                <button style={{padding:"3px 8px ", marginRight:"5px"}}>Edit</button>
-                 <button style={{padding:"3px 8px "}} onClick={()=>handleDelete(r.id)}>Delete</button>
-                  </div>
-          ))}
-          </div>
+                    <button style={{ padding: "5px 10px " }} type="submit">
+                        {editingId ?}"Update Recipe": "Add Recipe"</button>
+                </form>
+            </div>
+            <div style={{ maxWidth: "600px", margin: "20px auto", border: "1px solid #ccc", padding: "30px" }}>
+                <h3>All Recipes</h3>
+                {recipes.length === 0 && <p>No recipes Added yet</p>}
+                {recipes.map((r) => (
+                    <div key={r.id} style={{ border: "1px solid #ccc", padding: "10px", marginBottom: "10px" }}>
+                        <h4>Recipe Name :{r.name}</h4>
+                        <p><strong>Ingredients:</strong></p>
+                        <ul>{r.ingredients}</ul>
+                        <p><strong>Instructions:{r.instructions}</strong></p>
+                        <button style={{ padding: "3px 8px ", marginRight: "5px" }} onClick={() => handleEdit(r)}>Edit</button>
+                        <button style={{ padding: "3px 8px " }} onClick={() => handleDelete(r.id)}>Delete</button>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
